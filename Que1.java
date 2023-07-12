fa49cddda47f8a2560f547c1a5d40e9ca9085db5
ritikmishra14 <ritikmishra142001@gmail.com>
2023-07-13 00:01:01 +0530

    adding a new class

---
+++ Que1.java
@@ -0,0 +1,17 @@
+
+public class Que1 {
+
+	public static void main(String[] args) {
+		int a=8;
+		int b=9;
+		int c=10;
+		System.out.println(a+b+c);
+		int sum=a+b+c;
+		System.out.println(sum);
+		int d=11 , e=12, f=13;
+		int sum1=d+e+f;
+		System.out.println(sum1);
+		
+	}
+
+}
