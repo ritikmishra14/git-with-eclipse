fd482b86fcb6e12f6cbb11a18e9260bff744b2b2
ritikmishra14 <ritikmishra142001@gmail.com>
2023-07-13 14:52:03 +0530

    prime number code

---
+++ Divide.java
@@ -0,0 +1,18 @@
+public class Divide {
+  public static void main(String[] args) {
+    int number = 976;
+    boolean flag = false;
+    for (int i = 2; i <= number / 2; i++) {
+      if (number % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      System.out.println("Not a prime");
+
+    } else {
+      System.out.println("Is a prime");
+    }
+  }
+}
